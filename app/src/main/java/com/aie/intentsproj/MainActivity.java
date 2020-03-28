acef5050e9cc73de20f8b42683c135e3a3f827cb
isharaJayarathna <lakminijayarathna22@gmail.com>
2020-03-28 12:14:53 +0530

    commit message

---
+++ app/src/main/java/com/aie/intentsproj/MainActivity.java
@@ -0,0 +1,50 @@
+package com.aie.intentsproj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button click;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = findViewById(R.id.btnMain);
+
+
+
+        click.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent mainIntent = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(mainIntent);
+
+                LayoutInflater li = getLayoutInflater();
+                View layout = li.inflate(R.layout.customtoast, (ViewGroup)findViewById(R.id.custom_toast_layout));
+
+                //Creating the Toast object
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setView(layout);//setting the view of custom toast layout
+                toast.show();
+
+            }
+        });
+
+    }
+
+}
